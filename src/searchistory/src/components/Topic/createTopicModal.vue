0f71327c1065869c399018de71ffcda3c456435c
shobaseprg <pgymlsho19128@gmail.com>
2022-01-20 00:01:59 +0900

    movan

--- src/searchistory/src/components/Topic/createTopicModal.vue
+++ src/searchistory/src/components/Topic/createTopicModal.vue
@@ -5,13 +5,18 @@
     class="z-[1] w-[100%] h-[100%] bg-opacity-[0.5] fixed left-0 top-0 flex items-center justify-center bg-black"
   >
     <div class="z-[2] w-[50%] p-[1em] bg-white" @click="stopEvent">
-      <div>モーダル</div>
+      <div>
+        <mavon-editor :toolbars="markdownOption" language="en" v-model="topicContent" />
+      </div>
       <button @click="controlModal(false)">閉じる</button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import 'mavon-editor/dist/css/index.css'
+import { ref } from "vue";
+
 interface Props {
   controlModal: (flag: boolean) => void
 }
@@ -19,6 +24,45 @@ const { controlModal } = defineProps<Props>();
 const stopEvent = (event: any) => {
   event.stopPropagation();
 };
+
+const topicContent = ref("")
+
+const markdownOption = {
+  bold: true,
+  italic: true,
+  header: true,
+  underline: true,
+  strikethrough: true,
+  mark: true,
+  superscript: true,
+  subscript: true,
+  quote: true,
+  ol: true,
+  ul: true,
+  link: true,
+  imagelink: true,
+  code: true,
+  table: true,
+  fullscreen: true,
+  readmodel: true,
+  htmlcode: true,
+  help: true,
+  /* 1.3.5 */
+  undo: true,
+  redo: true,
+  trash: true,
+  save: true,
+  /* 1.4.2 */
+  navigation: true,
+  /* 2.1.8 */
+  alignleft: true,
+  aligncenter: true,
+  alignright: true,
+  /* 2.2.1 */
+  subfield: true,
+  preview: true,
+};
+
 </script>
 
 <style lang="" scoped>
--- src/searchistory/src/main.ts
+++ src/searchistory/src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
 import "./index.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(mavonEditor).mount("#app");
